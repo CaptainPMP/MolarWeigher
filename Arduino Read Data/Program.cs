080c556f9be4d682155af6be2ff37ebc8d4cba49
CaptainPMP <scikung8@gmail.com>
2020-09-30 07:55:18 +0700

    First Commit

---
+++ Arduino Read Data/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Arduino_Read_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.PortName = "COM4";
+            myport.Open();
+
+            while(true)
+            {
+                string data_rx = myport.ReadLine();
+                Console.WriteLine(data_rx);
+            }
+        }
+    }
+}
